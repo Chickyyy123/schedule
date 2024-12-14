aad4725a2e329a2d63655f96d13dc072e1c73aa9
Kimhak <heangkimhak11@gmail.com>
2024-12-15 01:16:26 +0700

    first commit

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,28 @@
 self.addEventListener('install', (e) => {
-    e.waitUntil(
+  e.waitUntil(
       caches.open('daily-schedule-v1').then((cache) => {
-        return cache.addAll([
-          './',
-          './index.html',
-          './manifest.json',
-          './idea.png',
-        ]);
+          // Cache files
+          return cache.addAll([
+              './',
+              './index.html',
+              './manifest.json',
+              './idea.png',
+          ]);
       })
-    );
-  });
-  
-  self.addEventListener('fetch', (e) => {
-    e.respondWith(
-      caches.match(e.request).then((response) => {
-        return response || fetch(e.request);
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  const cacheWhitelist = ['daily-schedule-v1'];
+  e.waitUntil(
+      caches.keys().then((cacheNames) => {
+          return Promise.all(
+              cacheNames.map((cacheName) => {
+                  if (!cacheWhitelist.includes(cacheName)) {
+                      return caches.delete(cacheName);
+                  }
+              })
+          );
       })
-    );
-  });
-  
+  );
+});
